1fd66e88543f1ff1b3a597f79c850d74649f4685
Eric McElroy <c-emcelroy@kinaxis.com>
2022-05-18 19:43:42 -0400

    fix resource group name reference to match other modules

--- azure/modules/more_resources/main.tf
+++ azure/modules/more_resources/main.tf
@@ -3,7 +3,7 @@ data "azurerm_client_config" "current" {}
 resource "azurerm_key_vault" "example_keyvault" {
   name                        = "${var.prefix}-keyvault"
   location                    = var.region
-  resource_group_name         = var.resource_group
+  resource_group_name         = var.resource_group_name
   enabled_for_disk_encryption = true
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days  = 7
--- azure/modules/more_resources/variables.tf
+++ azure/modules/more_resources/variables.tf
@@ -4,7 +4,7 @@ variable "region" {
   default     = "West US 2"
 }
 
-variable "resource_group" {
+variable "resource_group_name" {
   type        = string
   description = "resource group to use"
 }
